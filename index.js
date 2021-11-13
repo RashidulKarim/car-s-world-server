52da9c79c453fe85eb74319634b320639529f906
rashidulkarim <rashidul.karim7@gmail.com>
2021-11-13 23:39:07 +0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
-const app = express();
 const port = process.env.PORT || 5000;
 const ObjectId = require('mongodb').ObjectId
 require('dotenv').config();
